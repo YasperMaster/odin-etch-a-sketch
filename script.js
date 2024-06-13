eb1fe2df1c8cc13462daefe679fa68e916e0118c
Tomas Yasparra <tomasyas@gmail.com>
2024-06-13 08:17:24 -0300

    create the 16x16 grid and add the mouse trail

---
+++ script.js
@@ -0,0 +1,15 @@
+const container = document.querySelector("#container");
+
+for (let i = 0; i < 16; i++) {
+  for (let j = 0; j < 16; j++) {
+    const square = document.createElement("div");
+    square.classList.add("square");
+    container.appendChild(square);
+  }
+}
+
+square.addEventListener("mouseover", (event) => {
+  const colorSqr = document.createElement("div");
+  colorSqr.classList.add("coloredSquare");
+  container.appendChild(colorSqr);
+});
